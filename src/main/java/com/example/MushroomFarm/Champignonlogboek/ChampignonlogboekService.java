f383f334bde08d71df9b0b082278893794048eb6
kltavans <57217187+kltavans@users.noreply.github.com>
2021-01-08 15:36:32 +0100

    getAlleMeting methode gefixt

--- src/main/java/com/example/MushroomFarm/Champignonlogboek/ChampignonlogboekService.java
+++ src/main/java/com/example/MushroomFarm/Champignonlogboek/ChampignonlogboekService.java
@@ -3,6 +3,7 @@ package com.example.MushroomFarm.Champignonlogboek;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Service
--- src/main/java/com/example/MushroomFarm/Postgres.java
+++ src/main/java/com/example/MushroomFarm/Postgres.java
@@ -2,36 +2,27 @@ package com.example.MushroomFarm;
 import java.sql.*;
 
 public class Postgres {
-
-    public static void main(String[] args) {
+	
+	public void getAlleMetingen()
+	{
         try {
-            Class.forName("org.postgresql.Driver");
-        }
-        catch (java.lang.ClassNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
-
-        String url = "jdbc:postgresql://kandula.db.elephantsql.com:5432/hvicfmtr";
-        String username = "hvicfmtr";
-        String password = "Qb-3s4yhaWdhO4PipOrxyeolp27vwYXD";
-
-        try {
-            Connection db = DriverManager.getConnection(url, username, password);
-            Statement st = db.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM testmeting");
-            while (rs.next()) {
-                System.out.print("metingid ");
-                System.out.println(rs.getString(1));
-                System.out.print("Column 2 returned ");
-                System.out.println(rs.getString(2));
-                System.out.print("Column 3 returned ");
-                System.out.println(rs.getString(3));
-                System.out.print("Column 4 returned ");
-                System.out.println(rs.getString(4));
-                System.out.print("Column 5 returned ");
-                System.out.println(rs.getString(5));
-                System.out.print("Column 6 returned ");
-                System.out.println(rs.getString(6));
+    		  Connection con = ConnectionManager.getConnection();
+    		  Statement st = con.createStatement();
+              ResultSet rs = st.executeQuery("SELECT * FROM testmeting");
+              while (rs.next()) {
+                  System.out.print("metingid ");
+                  System.out.println(rs.getString(1));
+                  System.out.print("Column 2 returned ");
+                  System.out.println(rs.getString(2));
+                  System.out.print("Column 3 returned ");
+                  System.out.println(rs.getString(3));
+                  System.out.print("Column 4 returned ");
+                  System.out.println(rs.getString(4));
+                  System.out.print("Column 5 returned ");
+                  System.out.println(rs.getString(5));
+                  System.out.print("Column 6 returned ");
+                  System.out.println(rs.getString(6));
+    		 
             } 
             rs.close();
             st.close();
